3c913340310279d258899e25fc9dcc7e8c0b3b84
Пользователь <@USER>
2023-12-06 12:26:12 +0300

    Добавьте файлы проекта.

---
+++ ServerMarat/Server.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ServerMarat
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ButtonForGettingAnswers = new System.Windows.Forms.Button();
+            this.ButtonForUploadingQuestions = new System.Windows.Forms.Button();
+            this.ButtonToConnect = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainPanel.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.MainPanel.Controls.Add(this.label1);
+            this.MainPanel.Controls.Add(this.ButtonForGettingAnswers);
+            this.MainPanel.Controls.Add(this.ButtonForUploadingQuestions);
+            this.MainPanel.Controls.Add(this.ButtonToConnect);
+            this.MainPanel.Controls.Add(this.menuStrip1);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(800, 450);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(220, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Серверное главное меню";
+            // 
+            // ButtonForGettingAnswers
+            // 
+            this.ButtonForGettingAnswers.Location = new System.Drawing.Point(336, 298);
+            this.ButtonForGettingAnswers.Name = "ButtonForGettingAnswers";
+            this.ButtonForGettingAnswers.Size = new System.Drawing.Size(150, 30);
+            this.ButtonForGettingAnswers.TabIndex = 2;
+            this.ButtonForGettingAnswers.Text = "Получить ответы";
+            this.ButtonForGettingAnswers.UseVisualStyleBackColor = true;
+            this.ButtonForGettingAnswers.Click += new System.EventHandler(this.ButtonForGettingAnswers_Click);
+            // 
+            // ButtonForUploadingQuestions
+            // 
+            this.ButtonForUploadingQuestions.Location = new System.Drawing.Point(336, 262);
+            this.ButtonForUploadingQuestions.Name = "ButtonForUploadingQuestions";
+            this.ButtonForUploadingQuestions.Size = new System.Drawing.Size(150, 30);
+            this.ButtonForUploadingQuestions.TabIndex = 1;
+            this.ButtonForUploadingQuestions.Text = "Загрузить вопросы";
+            this.ButtonForUploadingQuestions.UseVisualStyleBackColor = true;
+            this.ButtonForUploadingQuestions.Click += new System.EventHandler(this.ButtonForUploadingQuestions_Click);
+            // 
+            // ButtonToConnect
+            // 
+            this.ButtonToConnect.Location = new System.Drawing.Point(336, 226);
+            this.ButtonToConnect.Name = "ButtonToConnect";
+            this.ButtonToConnect.Size = new System.Drawing.Size(150, 30);
+            this.ButtonToConnect.TabIndex = 0;
+            this.ButtonToConnect.Text = "Подключится";
+            this.ButtonToConnect.UseVisualStyleBackColor = true;
+            this.ButtonToConnect.Click += new System.EventHandler(this.ButtonToConnect_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem});
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MainPanel);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Server";
+            this.Text = "Сервер";
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Button ButtonForGettingAnswers;
+        private System.Windows.Forms.Button ButtonForUploadingQuestions;
+        private System.Windows.Forms.Button ButtonToConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+    }
+}
+
---
+++ ServerMarat/Server.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace ServerMarat
+{
+    public partial class Server : Form
+    {
+        TcpListener listener;
+
+        private string _answerAndQuestion = null;
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonToConnect_Click(object sender, EventArgs e)
+        {
+            ReadTheAnswerAndQuestion();
+            SendQuestionAndAnswer();
+
+        }
+        private void ButtonForUploadingQuestions_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void ButtonForGettingAnswers_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public async void SendQuestionAndAnswer()
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
+            listener = new TcpListener(ipPoint);
+            try
+            {
+                
+               listener.Start();
+                MessageBox.Show("Сервер запущен");
+               TcpClient handler = await listener.AcceptTcpClientAsync();
+               NetworkStream stream = handler.GetStream();
+                stream.Write(Encoding.UTF8.GetBytes(_answerAndQuestion),0, _answerAndQuestion.Length);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void ReadTheAnswerAndQuestion()
+        {
+            
+            string name;
+            try
+            {
+                StreamReader sr = new StreamReader(@"C:\Users\Пользователь\Source\Repos\Laboratory_Number_2\Laboratory_Number_1\QuestionAndAnswer\QuestionAndAnswer.txt");
+                for (int i = 0; i < 5; i++)
+                {
+                    name = sr.ReadLine();
+
+                    _answerAndQuestion += name;
+                    _answerAndQuestion += '\n';
+                   
+
+                };
+                MessageBox.Show(_answerAndQuestion);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        
+    }
+
+}
